3430c9729e89884650e1156a1d2b423bc99fda9c
gokcaneren <gokcann01@gmail.com>
2022-05-27 23:31:24 +0300

    DbContext and Configurations added!

---
+++ NLayer.Repository/Configurations/ProductConfiguration.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using NLayer.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NLayer.Repository.Configurations
+{
+    internal class ProductConfiguration : IEntityTypeConfiguration<Product>
+    {
+        public void Configure(EntityTypeBuilder<Product> builder)
+        {
+            builder.HasKey(x => x.Id);
+            builder.Property(x=>x.Id).UseIdentityColumn();
+            builder.Property(x=>x.Name).IsRequired().HasMaxLength(200);
+            builder.Property(x => x.Stock).IsRequired();
+            builder.Property(x => x.UnitPrice).IsRequired().HasColumnType("decimal(18,2)");
+
+            builder.HasOne(c => c.Category).WithMany(p => p.Products).HasForeignKey(p => p.CategoryId);
+        }
+    }
+}
