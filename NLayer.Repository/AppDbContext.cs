1444d0ca62a579bd3d7f4dc8781ab432f8e3f4a3
gokcaneren <gokcann01@gmail.com>
2022-05-30 21:27:43 +0300

    Data Seeds added!

--- NLayer.Repository/AppDbContext.cs
+++ NLayer.Repository/AppDbContext.cs
@@ -23,6 +23,27 @@ namespace NLayer.Repository
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+
+            modelBuilder.Entity<ProductFeature>().HasData(
+                new ProductFeature
+                {
+                    Id = 1,
+                    Color ="Kırmızı",
+                    Height=100,
+                    Width=50,
+                    ProductId=1
+                },
+                new ProductFeature
+                {
+                    Id = 2,
+                    Color = "Mavi",
+                    Height = 100,
+                    Width = 75,
+                    ProductId = 2
+                }
+                );
+
+
             base.OnModelCreating(modelBuilder);
         }
     }
