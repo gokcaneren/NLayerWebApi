a87c93cbe4ad6f8b2816523234fc33924d2aba63
gokcaneren <gokcann01@gmail.com>
2022-06-06 23:11:35 +0300

    CategoryId with Products endpoint is completed!

--- NLayer.Api/Program.cs
+++ NLayer.Api/Program.cs
@@ -20,8 +20,13 @@ builder.Services.AddSwaggerGen();
 builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
 builder.Services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
 builder.Services.AddScoped(typeof(IService<>), typeof(Service<>));
+
 builder.Services.AddScoped<IProductRepository, ProductRepository>();
 builder.Services.AddScoped<IProductService, ProductService>();
+
+builder.Services.AddScoped<ICategoryRepository, CategoryRepository>();
+builder.Services.AddScoped<ICategoryService, CategoryService>();
+
 builder.Services.AddAutoMapper(typeof(MapProfile));
 
 builder.Services.AddDbContext<AppDbContext>(x =>
--- NLayer.Service/Mapping/MapProfile.cs
+++ NLayer.Service/Mapping/MapProfile.cs
@@ -20,6 +20,7 @@ namespace NLayer.Service.Mapping
             CreateMap<ProductFeature, ProductFeatureDto>().ReverseMap();
             CreateMap<ProductUpdateDto, Product>();
             CreateMap<Product, ProductWithCategoryDto>();
+            CreateMap<Category, CategoryWithProductsDto>();
         }
     }
 }
