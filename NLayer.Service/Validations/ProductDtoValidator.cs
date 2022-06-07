dcef83a988150b5ada7ac66fd28bbb9167e94b54
gokcaneren <gokcann01@gmail.com>
2022-06-07 13:52:02 +0300

    Filter added!

--- NLayer.Service/Validations/ProductDtoValidator.cs
+++ NLayer.Service/Validations/ProductDtoValidator.cs
@@ -13,7 +13,7 @@ namespace NLayer.Service.Validations
         public ProductDtoValidator()
         {
             RuleFor(x => x.Name).NotNull().WithMessage("{PropertyName} is required!").NotEmpty().WithMessage(
-                "{PropertyName is required!}");
+                "{PropertyName} is required!");
             RuleFor(x => x.UnitPrice).InclusiveBetween(1, int.MaxValue).WithMessage("{PropertyName} must be greater than 0");
             RuleFor(x => x.Stock).InclusiveBetween(1, int.MaxValue).WithMessage("{PropertyName} must be greater than 0");
             RuleFor(x => x.CategoryId).InclusiveBetween(1, int.MaxValue).WithMessage("{PropertyName} must be greater than 0");
