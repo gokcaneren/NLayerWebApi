8eedda6facc78d2c3d07952f0148d90137956905
gokcaneren <gokcann01@gmail.com>
2022-06-10 17:30:31 +0300

    Module added!

---
+++ NLayer.Api/Modules/RepoServiceModule.cs
@@ -0,0 +1,25 @@
+﻿using Autofac;
+using NLayer.Repository;
+using NLayer.Service.Mapping;
+using System.Reflection;
+using Module = Autofac.Module; //Önemliiii
+namespace NLayer.Api.Modules
+{
+    public class RepoServiceModule:Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            var apiAssembly=Assembly.GetExecutingAssembly();
+            var repoAssembly = Assembly.GetAssembly(typeof(AppDbContext));
+            var serviceAssembly = Assembly.GetAssembly(typeof(MapProfile));
+
+
+            builder.RegisterAssemblyTypes(apiAssembly, repoAssembly, serviceAssembly).Where(
+                x => x.Name.EndsWith("Repository")).AsImplementedInterfaces().InstancePerLifetimeScope();
+
+            //InstancePerLifetimeScope => Scope
+            //InstancePerDependency => Transient
+
+        }
+    }
+}
